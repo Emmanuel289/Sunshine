6ae79ebc41ce11e2fcd2a2174b00d35361d2fc75
Emmanuel Ochia <emmirald@gmail.com>
2015-03-24 22:28:56 +0100

    I added SettingsActivity which is accessible from the Home Page(MainActivity) and DetailActivity action bar overflow button. This SettingsActivity displays a List of  editable user preferences in the application.

--- app/src/main/java/com/example/emmanuel/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/emmanuel/sunshine/app/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
+            startActivity(new Intent(this, SettingsActivity.class)); // create a new explicit intent to the SettingsActivity class
             return true;
         }
 
